c553cb2e9f0bbfd2951bbbc73113578f16e4a2af
Dean Zhao <rcarlosdasilva@qq.com>
2022-11-30 16:20:02 +0800

    refactor: 在spring中ApplicationContextInitializer的顺序

--- src/main/kotlin/cn/labzen/logger/spring/LabzenLoggerInitializer.kt
+++ src/main/kotlin/cn/labzen/logger/spring/LabzenLoggerInitializer.kt
@@ -3,6 +3,7 @@ package cn.labzen.logger.spring
 import cn.labzen.logger.meta.LabzenMetaPrinter
 import cn.labzen.logger.meta.LoggerConfiguration
 import cn.labzen.meta.Labzens
+import cn.labzen.meta.spring.SpringApplicationContextInitializerOrder
 import org.springframework.context.ApplicationContextInitializer
 import org.springframework.context.ConfigurableApplicationContext
 import org.springframework.core.Ordered
@@ -10,7 +11,7 @@ import org.springframework.core.Ordered
 class LabzenLoggerInitializer : ApplicationContextInitializer<ConfigurableApplicationContext>, Ordered {
 
   override fun getOrder(): Int =
-    Int.MIN_VALUE + 20
+    SpringApplicationContextInitializerOrder.MODULE_LOGGER_INITIALIZER_ORDER
 
   override fun initialize(applicationContext: ConfigurableApplicationContext) {
     val configuration = Labzens.configurationWith(LoggerConfiguration::class.java)
